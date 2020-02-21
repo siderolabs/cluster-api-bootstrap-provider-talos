0e90f4fe3f052858b85fdbb4ce0563010a9afcef
Spencer Smith <robertspencersmith@gmail.com>
2020-02-20 19:41:41 -0500

    feat: allow for BYO-MachineConfigs
    This PR will allow for users to do some far more complex Talos
    deployments with our CAPI components. You can now specify `none` for the generateType falue, then pre-generate configs
    with `osctl config generate` and pop them right into the `data` field of
    the TalosConfig resource.
    
    Signed-off-by: Spencer Smith <robertspencersmith@gmail.com>

--- controllers/talosconfig_controller.go
+++ controllers/talosconfig_controller.go
@@ -31,6 +31,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	capiv1 "sigs.k8s.io/cluster-api/api/v1alpha2"
 	"sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/cluster-api/util/patch"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -47,6 +48,17 @@ type TalosConfigReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
+}
+
+type TalosConfigScope struct {
+	Config  *bootstrapv1alpha2.TalosConfig
+	Machine *capiv1.Machine
+	Cluster *capiv1.Cluster
+}
+
+type TalosConfigBundle struct {
+	BoostrapData string
+	TalosConfig  string
 }
 
 type talosConfig struct {
@@ -138,110 +150,38 @@ func (r *TalosConfigReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, rerr
 		return ctrl.Result{}, errors.New("infra not ready")
 	}
 
-	// Determine what type of node this is
-	machineType := configmachine.TypeWorker
-	switch config.Spec.MachineType {
-	case "init":
-		machineType = configmachine.TypeInit
-	case "controlplane":
-		machineType = configmachine.TypeControlPlane
+	tcScope := &TalosConfigScope{
+		Config:  config,
+		Machine: machine,
+		Cluster: cluster,
 	}
 
-	APIEndpointPort := strconv.Itoa(cluster.Status.APIEndpoints[0].Port)
-	input, err := generate.NewInput(cluster.ObjectMeta.Name,
-		"https://"+cluster.Status.APIEndpoints[0].Host+":"+APIEndpointPort,
-		*machine.Spec.Version,
-	)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
+	var retData *TalosConfigBundle
 
-	inputSecret, err := r.fetchInputSecret(ctx, config, cluster.ObjectMeta.Name)
-	if machineType == configmachine.TypeInit && k8serrors.IsNotFound(err) {
-		inputSecret, err = r.writeInputSecret(ctx, config, cluster.ObjectMeta.Name, input)
+	switch config.Spec.GenerateType {
+	// Slurp and use user-supplied configs
+	case "none":
+		if config.Spec.Data == "" {
+			return ctrl.Result{}, errors.New("failed to specify config data with none generate type")
+		}
+		retData, err = r.userConfigs(ctx, tcScope)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
-	} else if err != nil {
-		return ctrl.Result{}, err
-	}
 
-	certs := &generate.Certs{}
-	kubeSecrets := &generate.Secrets{}
-	trustdInfo := &generate.TrustdInfo{}
-
-	err = yaml.Unmarshal(inputSecret.Data["certs"], certs)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	err = yaml.Unmarshal(inputSecret.Data["kubeSecrets"], kubeSecrets)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	err = yaml.Unmarshal(inputSecret.Data["trustdInfo"], trustdInfo)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	input.Certs = certs
-	input.Secrets = kubeSecrets
-	input.TrustdInfo = trustdInfo
-
-	input.InstallImage = config.Spec.InstallImage
-	input.InstallDisk = config.Spec.InstallDisk
-
-	nicList := []configmachine.Device{}
-	for _, nic := range config.Spec.NetworkInterfaces {
-		temp := configmachine.Device{}
-		temp.Interface = nic.Interface
-		temp.Ignore = nic.Ignore
-		nicList = append(nicList, temp)
-	}
-	input.NetworkConfig.NetworkInterfaces = nicList
-
-	talosConfig := &talosConfig{
-		Context: input.ClusterName,
-		Contexts: map[string]*talosConfigContext{
-			input.ClusterName: {
-				Target: "",
-				CA:     base64.StdEncoding.EncodeToString(input.Certs.OS.Crt),
-				Crt:    base64.StdEncoding.EncodeToString(input.Certs.Admin.Crt),
-				Key:    base64.StdEncoding.EncodeToString(input.Certs.Admin.Key),
-			},
-		},
-	}
-
-	talosConfigBytes, err := yaml.Marshal(talosConfig)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	data, err := generate.Config(machineType, input)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	if len(config.Spec.CNIUrls) > 0 {
-		cniOverride := &v1alpha1.CNIConfig{
-			CNIName: "custom",
-			CNIUrls: config.Spec.CNIUrls,
+	// Generate configs on the fly
+	case "init", "controlplane", "worker":
+		retData, err = r.genConfigs(ctx, tcScope)
+		if err != nil {
+			return ctrl.Result{}, err
 		}
-		data.ClusterConfig.ClusterNetwork.CNI = cniOverride
+
+	default:
+		return ctrl.Result{}, errors.New("unknown generate type specified")
 	}
 
-	// TODO: check that these are required in capi cluster types
-	data.ClusterConfig.ClusterNetwork.PodSubnet = cluster.Spec.ClusterNetwork.Pods.CIDRBlocks
-	data.ClusterConfig.ClusterNetwork.ServiceSubnet = cluster.Spec.ClusterNetwork.Services.CIDRBlocks
-
-	dataOut, err := data.String()
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	config.Status.BootstrapData = []byte(dataOut)
-	config.Status.TalosConfig = string(talosConfigBytes)
+	config.Status.BootstrapData = []byte(retData.BoostrapData)
+	config.Status.TalosConfig = retData.TalosConfig
 	config.Status.Ready = true
 
 	return ctrl.Result{}, nil
@@ -249,7 +189,7 @@ func (r *TalosConfigReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, rerr
 
 func (r *TalosConfigReconciler) reconcileDelete(ctx context.Context, config *bootstrapv1alpha2.TalosConfig, clusterName string) (ctrl.Result, error) {
 
-	if config.Spec.MachineType == "init" {
+	if config.Spec.GenerateType == "init" {
 		err := r.deleteInputSecret(ctx, config, clusterName)
 		if err != nil {
 			return ctrl.Result{}, err
@@ -262,9 +202,135 @@ func (r *TalosConfigReconciler) reconcileDelete(ctx context.Context, config *boo
 	return ctrl.Result{}, nil
 
 }
+
 func (r *TalosConfigReconciler) SetupWithManager(mgr ctrl.Manager, options controller.Options) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		WithOptions(options).
 		For(&bootstrapv1alpha2.TalosConfig{}).
 		Complete(r)
+}
+
+func genTalosConfigFile(clusterName string, certs *generate.Certs) (string, error) {
+	talosConfig := &talosConfig{
+		Context: clusterName,
+		Contexts: map[string]*talosConfigContext{
+			clusterName: {
+				Target: "",
+				CA:     base64.StdEncoding.EncodeToString(certs.OS.Crt),
+				Crt:    base64.StdEncoding.EncodeToString(certs.Admin.Crt),
+				Key:    base64.StdEncoding.EncodeToString(certs.Admin.Key),
+			},
+		},
+	}
+
+	talosConfigBytes, err := yaml.Marshal(talosConfig)
+	if err != nil {
+		return "", err
+	}
+
+	return string(talosConfigBytes), nil
+}
+
+// userConfigs will fetch and make use of user-supplied bootstrap configs to return
+func (r *TalosConfigReconciler) userConfigs(ctx context.Context, scope *TalosConfigScope) (*TalosConfigBundle, error) {
+	retBundle := &TalosConfigBundle{}
+
+	userConfig := &v1alpha1.Config{}
+	err := yaml.Unmarshal([]byte(scope.Config.Spec.Data), userConfig)
+	if err != nil {
+		return retBundle, err
+	}
+
+	userConfigStr, err := userConfig.String()
+	if err != nil {
+		return retBundle, err
+	}
+
+	retBundle.BoostrapData = userConfigStr
+
+	return retBundle, nil
+}
+
+// genConfigs will generate a bootstrap config and a talosconfig to return
+func (r *TalosConfigReconciler) genConfigs(ctx context.Context, scope *TalosConfigScope) (*TalosConfigBundle, error) {
+	retBundle := &TalosConfigBundle{}
+
+	// Determine what type of node this is
+	machineType := configmachine.TypeWorker
+	switch scope.Config.Spec.GenerateType {
+	case "init":
+		machineType = configmachine.TypeInit
+	case "controlplane":
+		machineType = configmachine.TypeControlPlane
+	}
+
+	APIEndpointPort := strconv.Itoa(scope.Cluster.Status.APIEndpoints[0].Port)
+	input, err := generate.NewInput(scope.Cluster.ObjectMeta.Name,
+		"https://"+scope.Cluster.Status.APIEndpoints[0].Host+":"+APIEndpointPort,
+		*scope.Machine.Spec.Version,
+	)
+	if err != nil {
+		return retBundle, err
+	}
+
+	inputSecret, err := r.fetchInputSecret(ctx, scope.Config, scope.Cluster.ObjectMeta.Name)
+	if machineType == configmachine.TypeInit && k8serrors.IsNotFound(err) {
+		inputSecret, err = r.writeInputSecret(ctx, scope.Config, scope.Cluster.ObjectMeta.Name, input)
+		if err != nil {
+			return retBundle, err
+		}
+	} else if err != nil {
+		return retBundle, err
+	}
+
+	certs := &generate.Certs{}
+	kubeSecrets := &generate.Secrets{}
+	trustdInfo := &generate.TrustdInfo{}
+
+	err = yaml.Unmarshal(inputSecret.Data["certs"], certs)
+	if err != nil {
+		return retBundle, err
+	}
+
+	err = yaml.Unmarshal(inputSecret.Data["kubeSecrets"], kubeSecrets)
+	if err != nil {
+		return retBundle, err
+	}
+
+	err = yaml.Unmarshal(inputSecret.Data["trustdInfo"], trustdInfo)
+	if err != nil {
+		return retBundle, err
+	}
+
+	input.Certs = certs
+	input.Secrets = kubeSecrets
+	input.TrustdInfo = trustdInfo
+
+	tcString, err := genTalosConfigFile(input.ClusterName, input.Certs)
+	if err != nil {
+		return retBundle, err
+	}
+
+	retBundle.TalosConfig = tcString
+
+	data, err := generate.Config(machineType, input)
+	if err != nil {
+		return retBundle, err
+	}
+
+	if scope.Cluster.Spec.ClusterNetwork.Pods != nil {
+		data.ClusterConfig.ClusterNetwork.PodSubnet = scope.Cluster.Spec.ClusterNetwork.Pods.CIDRBlocks
+	}
+	if scope.Cluster.Spec.ClusterNetwork.Services != nil {
+		data.ClusterConfig.ClusterNetwork.ServiceSubnet = scope.Cluster.Spec.ClusterNetwork.Services.CIDRBlocks
+	}
+
+	dataOut, err := data.String()
+	if err != nil {
+		return retBundle, err
+	}
+
+	retBundle.BoostrapData = dataOut
+
+	return retBundle, nil
 }
