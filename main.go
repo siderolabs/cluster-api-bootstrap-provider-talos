1d758ffed8362929788896b1e618be6384bb7384
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2022-02-02 21:32:08 +0300

    feat: add readiness/liveness checks
    This is using standard webhook-based checks.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>
    (cherry picked from commit b27f9765e87e9c3aa6730286579a4ca4ba05d384)

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	bootstrapv1alpha3 "github.com/talos-systems/cluster-api-bootstrap-provider-talos/api/v1alpha3"
 	"github.com/talos-systems/cluster-api-bootstrap-provider-talos/controllers"
@@ -29,6 +30,7 @@ var (
 	setupLog = ctrl.Log.WithName("setup")
 
 	metricsAddr          string
+	healthAddr           string
 	enableLeaderElection bool
 	webhookPort          int
 	watchFilterValue     string
@@ -46,6 +48,9 @@ func InitFlags(fs *pflag.FlagSet) {
 
 	fs.StringVar(&watchFilterValue, "watch-filter", "",
 		fmt.Sprintf("Label value that the controller watches to reconcile cluster-api objects. Label key is always %s. If unspecified, the controller watches for all cluster-api objects.", capiv1.WatchLabel))
+
+	flag.StringVar(&healthAddr, "health-addr", ":9440",
+		"The address the health endpoint binds to.")
 
 	feature.MutableGates.AddFlag(fs)
 }
@@ -68,20 +73,35 @@ func main() {
 	})
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		MetricsBindAddress: metricsAddr,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "controller-leader-election-cabpt",
-		Port:               webhookPort,
-		EventBroadcaster:   broadcaster,
+		MetricsBindAddress:     metricsAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "controller-leader-election-cabpt",
+		Port:                   webhookPort,
+		EventBroadcaster:       broadcaster,
+		HealthProbeBindAddress: healthAddr,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
 
-	ctx := context.Background()
+	ctx := ctrl.SetupSignalHandler()
 
-	if err = (&controllers.TalosConfigReconciler{
+	setupReconcilers(ctx, mgr)
+	setupWebhooks(mgr)
+	setupChecks(mgr)
+
+	// +kubebuilder:scaffold:builder
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctx); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
+
+func setupReconcilers(ctx context.Context, mgr manager.Manager) {
+	if err := (&controllers.TalosConfigReconciler{
 		Client:           mgr.GetClient(),
 		Log:              ctrl.Log.WithName("controllers").WithName("TalosConfig"),
 		Scheme:           mgr.GetScheme(),
@@ -90,21 +110,27 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "TalosConfig")
 		os.Exit(1)
 	}
+}
 
-	if err = (&bootstrapv1alpha3.TalosConfigTemplate{}).SetupWebhookWithManager(mgr); err != nil {
+func setupWebhooks(mgr manager.Manager) {
+	if err := (&bootstrapv1alpha3.TalosConfigTemplate{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "TalosConfigTemplate")
 		os.Exit(1)
 	}
-	if err = (&bootstrapv1alpha3.TalosConfig{}).SetupWebhookWithManager(mgr); err != nil {
+	if err := (&bootstrapv1alpha3.TalosConfig{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "TalosConfig")
 		os.Exit(1)
 	}
+}
 
-	// +kubebuilder:scaffold:builder
+func setupChecks(mgr ctrl.Manager) {
+	if err := mgr.AddReadyzCheck("webhook", mgr.GetWebhookServer().StartedChecker()); err != nil {
+		setupLog.Error(err, "unable to create ready check")
+		os.Exit(1)
+	}
 
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+	if err := mgr.AddHealthzCheck("webhook", mgr.GetWebhookServer().StartedChecker()); err != nil {
+		setupLog.Error(err, "unable to create health check")
 		os.Exit(1)
 	}
 }
